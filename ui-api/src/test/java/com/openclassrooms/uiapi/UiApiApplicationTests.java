a518f5aa2cc8f4da7b0324cbc9ae18d7af9f48eb
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2021-11-24 11:00:13 +0100

    first commit

---
+++ ui-api/src/test/java/com/openclassrooms/uiapi/UiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.uiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
